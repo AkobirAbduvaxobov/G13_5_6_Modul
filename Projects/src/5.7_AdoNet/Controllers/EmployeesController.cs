3455e2d47d8863f51ac95101b724fb5f2758cf53
Akobir <akobirabduvahobov@gmail.com>
2026-04-25 17:47:32 +0500

    p

---
+++ Projects/src/5.7_AdoNet/Controllers/EmployeesController.cs
@@ -0,0 +1,33 @@
+﻿using _5._7_AdoNet.Entities;
+using _5._7_AdoNet.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _5._7_AdoNet.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController()
+        {
+            _employeeService = new EmployeeService();
+        }
+
+        [HttpPost]
+        public async Task<long> AddEmployee(Employee employee)
+        {
+            var employeeId = await _employeeService.AddAsync(employee);
+            return employeeId;
+        }
+
+        [HttpGet]
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            var employees = await _employeeService.GetAllAsync();
+            return employees;
+        }
+    }
+}
---
+++ Projects/src/5_2_dars_leetcode/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _5_2_dars_leetcode;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string res = "Qovun";
+        Console.WriteLine(res.ToArray().Reverse().ToString());
+
+    }
+
+
+}
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        return null;
+    }
+
+    public string GetValuesAsString(ListNode lN)
+    {
+        var res = string.Empty;
+        while(lN != null)
+        {
+            res += lN.val;
+            lN = lN.next;
+        }
+
+        return res.ToArray().Reverse().ToString();
+    }
+
+    
+
+
+
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
